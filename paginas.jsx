5637277b725c7dff0ec2f65e793ad01c7df0cf20
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-10-23 14:55:54 -0300

    a

---
+++ paginas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Pag = () => {
+    return (
+        <Navbar />,
+        <div>
+            <h1>TEMA</h1>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+export default Pag
